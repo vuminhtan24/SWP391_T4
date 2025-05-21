012ac1c6047efb62cae67ba1c4d641927f7628ca
dangquangkha <126367262+dangquangkha@users.noreply.github.com>
2025-05-21 22:40:36 +0700

    View user detail

--- LaFioreria/src/java/dal/UserDAO.java
+++ LaFioreria/src/java/dal/UserDAO.java
@@ -9,18 +9,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Role;
 import model.User;
+import model.UserManager;
 
 /**
  *
  * @author ADMIN
  */
-public class UserDAO extends DBContext{
-    public List<User> getAll(){
+public class UserDAO extends DBContext {
+
+    public List<User> getAll() {
         List<User> listUser = new ArrayList<>();
-        
+
         String sql = "Select * from la_fioreria.user";
-        
+
         try {
             PreparedStatement pre = connection.prepareStatement(sql);
             ResultSet rs = pre.executeQuery();
@@ -39,8 +42,84 @@ public class UserDAO extends DBContext{
         } catch (SQLException e) {
             System.out.println(e);
         }
-        
+
         return listUser;
     }
+
+    public List<String> getRoleNames() {
+        List<String> listRole = new ArrayList<>();
+
+        String sql = "select Role_name from role;";
+
+        try {
+            PreparedStatement pre = connection.prepareStatement(sql);
+            ResultSet rs = pre.executeQuery();
+            
+            while(rs.next()){
+                String Role_name = rs.getString("Role_name");
+                listRole.add(Role_name);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return listRole;
+    }
     
+
+    
+    public List<Integer> getIds() {
+
+        List<Integer> ids = new ArrayList<>();
+        String sql = "select user_id from user;";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("user_id");
+                ids.add(id);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return ids;
+    }
+
+    public UserManager getUserById(int id) {
+        String sql = "select u.User_ID,u.Username,u.Password,u.Fullname,u.Email,u.Phone,u.Address,r.Role_name from user u join role r on u.Role = r.Role_id where u.User_ID = ?;";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String user_name = rs.getString("username");
+                String password = rs.getString("password");
+                String fullname = rs.getString("fullname");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                String address = rs.getString("address");
+                String role = rs.getString("role_name");
+                UserManager user = new UserManager(id, user_name, password, fullname, email, phone, address, role);
+                return user;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        UserDAO ud = new UserDAO();
+//        List<Integer> lst = ud.getIds();
+//        for (int i = 0; i < lst.size(); i++) {
+//            Integer get = lst.get(i);
+//            System.out.println(get);
+//        }
+//        UserManager um = ud.getUserById(1);
+//        System.out.println(um.toString());
+        
+    }
+
 }
---
+++ LaFioreria/src/java/model/UserManager.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class UserManager {
+
+    private int userid;
+    private String username;
+    private String password;
+    private String fullname;
+    private String email;
+    private String phone;
+    private String address;
+    private String role;
+
+    public UserManager() {
+    }
+
+    public UserManager(int userid, String username, String password, String fullname, String email, String phone, String address, String role) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserManager{" + "userid=" + userid + ", username=" + username + ", password=" + password + ", fullname=" + fullname + ", email=" + email + ", phone=" + phone + ", address=" + address + ", role=" + role + '}';
+    }
+    
+    
+    
+}
